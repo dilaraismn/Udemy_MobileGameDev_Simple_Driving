05fdbd5f6544d089c7c6c33160a09ac282d3f47c
Dilara <dilaraismn@gmail.com>
2021-09-29 21:32:08 +0300

    23. driving forward

---
+++ Assets/SimpleDriving/Car.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float speedGain = 0.2f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        speed += speedGain * Time.deltaTime;
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
