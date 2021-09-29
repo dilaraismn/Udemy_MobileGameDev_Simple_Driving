543402bb572aebf42711e2cd36bb71cedfb07fcb
Dilara <dilaraismn@gmail.com>
2021-09-29 22:20:04 +0300

    24. steering the car

--- Assets/SimpleDriving/Car.cs
+++ Assets/SimpleDriving/Car.cs
@@ -7,6 +7,10 @@ public class Car : MonoBehaviour
 
     [SerializeField] private float speed = 10f;
     [SerializeField] private float speedGain = 0.2f;
+    [SerializeField] private float turnSpeed = 200f;
+
+    private int steerValue; // if 0 not turning | if 1 turning to right | if -1 turning to left
+    
     void Start()
     {
         
@@ -16,6 +20,13 @@ public class Car : MonoBehaviour
     {
         speed += speedGain * Time.deltaTime;
 
+        transform.Rotate(0f, steerValue * turnSpeed * Time.deltaTime, 0f);
+            
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    public void Steer(int value)
+    {
+        steerValue = value;
     }
 }
