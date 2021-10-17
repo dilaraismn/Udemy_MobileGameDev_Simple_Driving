81a91b3de65bb2f4cbe27b01c9c2e21b988eddd8
Dilara <dilaraismn@gmail.com>
2021-10-17 22:39:54 +0300

    28. score system

---
+++ Assets/SimpleDriving/_Scripts/ScoreHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreHandler : MonoBehaviour
+{
+    [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private float scoreMultiplier;
+
+    private float score;
+    
+    void Update()
+    {
+        score += Time.deltaTime * scoreMultiplier;
+        // +1 score by 1 second => score += Time.deltaTime;
+
+        scoreText.text = Mathf.FloorToInt(score).ToString();
+        // to not show the decimal numbers
+    }
+}
