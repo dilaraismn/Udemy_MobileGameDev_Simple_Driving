0659f4f81b38ece7e488976f57a0e071801804dd
Dilara <dilaraismn@gmail.com>
2021-10-17 23:45:42 +0300

    30. energy system

--- Assets/SimpleDriving/_Scripts/MainMenu.cs
+++ Assets/SimpleDriving/_Scripts/MainMenu.cs
@@ -8,6 +8,14 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private TMP_Text highScoreText;
+    [SerializeField] private TMP_Text energyText;
+    [SerializeField] private int maxEnergy;
+    [SerializeField] private int energyRechargeDuration;
+
+    private int energy;
+
+    private const string EnergyKey = "Energy";
+    private const string EnergyReadyKey = "EnergyReady";
 
 
     private void Start()
@@ -17,10 +25,49 @@ public class MainMenu : MonoBehaviour
         highScoreText.text = $"High Score: {highScore}";
         //highScoreText.text = "High Score :" + highScore.ToString();
         // highScoreText.text = highScore.ToString();
+
+        energy = PlayerPrefs.GetInt(EnergyKey, maxEnergy);
+        //check how much energy we've got
+
+        if (energy == 0) //if we're out of energy
+        {
+            string energyReadyString = PlayerPrefs.GetString(EnergyReadyKey, string.Empty);
+            //check when energy should be regenerated
+
+            if (energyReadyString == string.Empty) return;
+
+            DateTime energyReady = DateTime.Parse(energyReadyString);
+            //find out when our energy will be regenerated and convert it 
+
+            if (DateTime.Now > energyReady) // compare two different date types - current and when it will be ready
+            {
+                //if its bigger we've already passed that time and our energy is ready so we set it to max
+                energy = maxEnergy;
+                PlayerPrefs.SetInt(EnergyKey, energy);
+            }
+        }
+
+
+        energyText.text = $"Play ({energy})";
     }
 
     public void Play()
     {
+        if(energy < 1) return;
+
+        energy--;
+        
+        PlayerPrefs.SetInt(EnergyKey, energy);
+
+        if (energy == 0)
+        {
+            DateTime energyReady = DateTime.Now.AddMinutes(energyRechargeDuration);
+            PlayerPrefs.SetString(EnergyReadyKey, energyReady.ToString());
+        }
+        
+        
+
+        
         SceneManager.LoadScene(1);
     }
 }
